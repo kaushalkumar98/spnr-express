69d33a8bb1727c56f0b9877bb8d84eff9646bfe0
kaushalkumar98 <kaushalkumar3795@gmail.com>
2023-11-17 21:54:23 +0530

    Task-5 Serving html, css & js(views)

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
+const contactRoutes = require('./routes/contact');
 const shopRoutes = require('./routes/shop');
+const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use('/admin', contactRoutes);
+app.use(shopRoutes);
+app.use(successRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); 
 })
 
 app.listen(3000);
--- routes/contact.js
+++ routes/contact.js
@@ -6,8 +6,9 @@ const rootDir = require('../util/path')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get('/contactus', (req, res, next) => {
+    res.sendFile(path.join(rootDir,'views', 'contact-us.html'));
 });
+ 
 
 module.exports = router;
